23210d7b6e92d9aa45d3c5c4f580fbb1bc075422
bkendzior <brian.kendzior@moovweb.com>
2012-08-23 15:57:27 -0700

    Stylesheet adjustments

--- scripts/pages/browse/category_image.ts
+++ scripts/pages/browse/category_image.ts
@@ -3,6 +3,52 @@ $("./body") {
 
   $(".//div[@id='main']") {
 
+    // Transform Breadcrumbs
+    $("./div[@id='breadcrumbs']") {
+      $(".//div[contains(@class,'blue')]") {
+        $("./a") {
+          move_to("../..", "top")
+        }
+
+        text() {
+          replace(/home\s>/i,"")
+          replace(/>/,"»")
+        }
+      }
+    }
+
+    $("./div[@id='content']") {
+    
+      // Replace the images with their alt_text
+      $("./img") {
+        $alt_text = fetch("@alt")
+        $("..") {
+          insert_top("a") {
+            add_class("mw_h2")
+            inject($alt_text)
+          }
+        }
+        remove()
+      }
+
+      // Bodybuilding
+      $("./div[@id='main-box']") {
+        $(".//img") {
+          $alt_text = fetch("@alt")
+          $("..") {
+            inject($alt_text)
+            insert_bottom("div")
+          }
+          remove()
+        }
+      }
+      
+      $(".//a[contains(@class,'clickbox')]") {
+        insert_bottom("div") // Inserted for the chevron icon 
+      }
+
+    }
+
     // Remove the menu
     $("./div[@id='menu']") {
       remove()
