9e06f0a3e7b400a673641d0aa468a8d4f10dba92
bkendzior <brian.kendzior@moovweb.com>
2012-10-12 01:28:29 -0700

    Style

--- scripts/define_faceboots.ts
+++ scripts/define_faceboots.ts
@@ -2,12 +2,12 @@ $("/html/body") {
   attribute("fb_connected", "false")
 
   insert_bottom("div", id:'fb-root', class:'fb-reset')
+  
+  faceboots_init()
+
+  faceboots_set_appID('411584758897232')
+  faceboots_set_appSecret('aa701787b407a8728584d751f8837236')
+  faceboots_set_appNamespace('faceboots')
+
+  faceboots_load_sdk()
 }
-
-faceboots_init()
-
-faceboots_set_appID('411584758897232')
-faceboots_set_appSecret('aa701787b407a8728584d751f8837236')
-faceboots_set_appNamespace('faceboots')
-
-faceboots_load_sdk()
